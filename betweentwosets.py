e9a8e941c244ed84ba6679e2eef26b98668b3407
Ryan <rway1@mail.csuchico.edu>
2017-08-03 15:18:47 -0700

    set up for between two sets problem

---
+++ betweentwosets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def getTotalX(a, b):
+    # Complete this function
+
+if __name__ == "__main__":
+    n, m = input().strip().split(' ')
+    n, m = [int(n), int(m)]
+    a = list(map(int, input().strip().split(' ')))
+    b = list(map(int, input().strip().split(' ')))
+    total = getTotalX(a, b)
+    print(total)
