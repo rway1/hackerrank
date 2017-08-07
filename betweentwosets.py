6ed3b39e83e328f3a442fc7e69f7a3afc9696300
Ryan <rway1@mail.csuchico.edu>
2017-08-06 19:00:44 -0700

    method headers, algorithm for between two sets

--- betweentwosets.py
+++ betweentwosets.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+#algorithm
+
+#1) L = LCM(a)
+#2) G = GCD(b)
+#3) F = factors(G)
+#4) solution = count of x in F, where x % l is 0
+
 import sys
 
+#calculates gcd of two integers
 def gcd(x, y):
 
    while(y):
@@ -9,26 +17,32 @@ def gcd(x, y):
 
    return x
 
+#calculates lcm of two integers 
 def lcm(x, y):
 
    lcm = (x*y)//gcd(x,y)
    return lcm
 
+#calculates gcd of list
 def Gcd(x):
   g = x[0]
   for i in x:
     g = gcd(g,i)
   return g
 
+#calculates lcm of list
 def Lcm(x):
   l = x[0]
   for i in x:
     l = lcm(l,i)
   return l
 
+#returns list of factors for integer
 def findFactors(x):
   return [ i for i in range(1,x+1) if x%i == 0]
 
+#calculates number of x, where x % a[i] == 0 for 0 <= i <= n
+# and b[i] % x == 0 for 0 <= i <= n
 def getTotalX(a, b):
   g = Gcd(b)
   l = Lcm(a)
