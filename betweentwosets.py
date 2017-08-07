d7b0aaffbeb1274cd8bf2ede5ef6700087238c5b
Ryan <rway1@mail.csuchico.edu>
2017-08-06 18:42:52 -0700

    between two sets solved

--- betweentwosets.py
+++ betweentwosets.py
@@ -2,8 +2,47 @@
 
 import sys
 
+def gcd(x, y):
+
+   while(y):
+       x, y = y, x % y
+
+   return x
+
+def lcm(x, y):
+
+   lcm = (x*y)//gcd(x,y)
+   return lcm
+
+def Gcd(x):
+  g = x[0]
+  for i in x:
+    g = gcd(g,i)
+  return g
+
+def Lcm(x):
+  l = x[0]
+  for i in x:
+    l = lcm(l,i)
+  return l
+
+def findFactors(x):
+  y = []
+  for i in range(1,x+1):
+    if x%i == 0:
+      y.append(i)
+  return y
+
 def getTotalX(a, b):
-    # Complete this function
+  g = Gcd(b)
+  l = Lcm(a)
+  z = findFactors(g)
+  total = 0
+  for i in z:
+    if i % l == 0:
+      total += 1
+
+  return total
 
 if __name__ == "__main__":
     n, m = input().strip().split(' ')
