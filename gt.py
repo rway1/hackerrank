25163ca17a03c6b1e1b790ccbc0bf891a903a148
rway1 <rway1@csuchico.edu>
2018-02-18 21:34:57 -0800

    reorganize, gt uses stdin and stdout

--- gt.py
+++ gt.py
@@ -4,40 +4,53 @@ import difflib
 import importlib
 import re
 import sys
+import filecmp
 
-file_to_be_tested = "executable"
+
+cwd = os.getcwd()
 test_directory_name = "tests"
+result_directory_name = "results"
+test_directory = cwd + "/" + test_directory_name
+file_to_be_tested = "executable"
 output_file_regex = "\w+.out"
 output_file_ext = ".out"
 input_file_regex = "\w+.in"
 input_file_ext = ".in"
+result_file_ext = ".myout"
+command = ["python3", "", "<", "", ">", ""]
 
 
 def main():
-    module = GetModule()
+    executable = GetExecutable()
     test_dictionary = GetTestDictionary()
-    print("Value from gt: " + value)
-
-def Print(args):
-    return args
+    i = 1
+    for input_file, output_file in test_dictionary.items():
+        result_file = input_file.replace(input_file_ext, result_file_ext).replace(test_directory_name, result_directory_name)
+        command[1] = executable
+        command[3] = input_file
+        command[5] = result_file
+        os.system(' '.join(command))
+        if filecmp.cmp(output_file, result_file):
+            print("test " + str(i) + " passed")
+        else:
+            print("test " + str(i) + " failed")
+        i = i + 1
 
 def GetTestDictionary():
-    cwd = os.getcwd()
-    test_directory = cwd + "/" + test_directory_name
     if not os.path.exists(test_directory):
         print("Test Directory Not Found")
         sys.exit()
         
     all_test_files = os.listdir(test_directory)
     test_in_files = [
-        file_name for file_name in all_test_files
+        test_directory_name + "/" + file_name for file_name in all_test_files
             if re.match(input_file_regex, file_name)
     ]
     
     test_dictionary = dict.fromkeys(test_in_files)
     for input_file in test_in_files:
         output_file = input_file.replace(input_file_ext, output_file_ext)
-        if output_file in all_test_files:
+        if os.path.exists(output_file):
             test_dictionary[input_file] = output_file
         else:
             print("No matching output file: " + output_file + " for: " + input_file)
@@ -45,18 +58,9 @@ def GetTestDictionary():
     return test_dictionary
 
 
-def GetModule():
+def GetExecutable():
     cwd = os.getcwd()
-    common_prefix = os.path.commonprefix([cwd, os.path.abspath(__file__)])
-    relative_path = cwd.replace(common_prefix, "")
-    import_string = relative_path.replace("/", ".") + "." + file_to_be_tested
-    try:
-        module = importlib.import_module(import_string)
-        module.Print = Print
-        return module    
-    except:
-        print("No Module Found: " + import_string)
-        sys.exit()
+    return cwd + "/" + file_to_be_tested + ".py"
 
 
 
--- implementation/bonappetit/executable.py
+++ implementation/bonappetit/executable.py
@@ -1,13 +1,5 @@
 import sys
 
-def ReturnAString():
-    return "String"
 
-def main(argv):
-    Print(argv)
 
-def Print(args):
-    print(args)
-
-if __name__ == "__main__":
-    main(sys.argv)
+print("hello world!")
