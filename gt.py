406364a6f206330e10fef13c8e6fd8c6582e3122
rway1 <rway1@csuchico.edu>
2018-02-18 19:36:13 -0800

    reorganize + add error checking for FindModule in gt

--- gt.py
+++ gt.py
@@ -3,24 +3,26 @@ from os import listdir
 import difflib
 import importlib
 import re
+import sys
 
 file_to_be_tested = "executable"
 
 
 def main():
+    module = FindModule()
+    module.RunCodeOnFile()
+
+def FindModule():
     cwd = os.getcwd()
     common_prefix = os.path.commonprefix([cwd, os.path.abspath(__file__)])
     relative_path = cwd.replace(common_prefix, "")
-    import_string = relative_path.replace("/", ".") + "." + file_to_be_tested
-    module = importlib.import_module(import_string)
-    module.RunCodeOnFile()
-    
+    try:
+        import_string = relative_path.replace("/", ".") + "." + file_to_be_tested
+        return importlib.import_module(import_string)
+    except:
+        print("No Module Found")
+        sys.exit()
 
-def FindExecutable():
-    for file in listdir(path=dir_path):
-        print(file)
-        if re.search(".py", file):
-            return file
 
 
 if __name__ == "__main__":
