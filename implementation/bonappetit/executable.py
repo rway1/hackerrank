afea99c98939ffb64a2537e0aa30fb1723a59c1f
rway1 <rway1@csuchico.edu>
2018-02-24 18:08:38 -0800

    one line if statement

--- implementation/bonappetit/executable.py
+++ implementation/bonappetit/executable.py
@@ -7,6 +7,5 @@ c = [int(i) for i in input().split(" ")]
 b = int(input())
 
 c.remove(c[k])
-if b == sum(c)/2:
-    sys.stdout.write("Bon Appetit")
+if b == sum(c)/2: sys.stdout.write("Bon Appetit")
 else: sys.stdout.write(str(int(b - sum(c)/2)))
