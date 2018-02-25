39141d9f4dafe2df08c34bb8fc6bdfcd10d145c0
rway1 <rway1@csuchico.edu>
2018-02-25 11:15:25 -0800

    finished sock merchant

--- implementation/sock-merchant/executable.py
+++ implementation/sock-merchant/executable.py
@@ -3,7 +3,16 @@
 import sys
 
 def sockMerchant(n, ar):
-    return n
+    counter_list = [0] * 101
+    for item in ar:
+        counter_list[item] = counter_list[item] + 1
+    
+    pair_count = 0
+
+    for item in counter_list:
+        pair_count = pair_count + int(item/2)
+    
+    return pair_count
 
 n = int(input().strip())
 ar = list(map(int, input().strip().split(' ')))
