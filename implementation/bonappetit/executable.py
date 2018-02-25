5049083c7bca35b6d7101c4a269c49c53ad501a8
rway1 <rway1@csuchico.edu>
2018-02-24 18:04:42 -0800

    bon appetit challenge solved

--- implementation/bonappetit/executable.py
+++ implementation/bonappetit/executable.py
@@ -2,4 +2,11 @@ import sys
 
 
 
-print("hello world!")
+(n, k) = [int(i) for i in input().split(" ")]
+c = [int(i) for i in input().split(" ")]
+b = int(input())
+
+c.remove(c[k])
+if b == sum(c)/2:
+    sys.stdout.write("Bon Appetit")
+else: sys.stdout.write(str(int(b - sum(c)/2)))
